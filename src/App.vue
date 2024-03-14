5f02e92b03fa1ec67ea686bd5ec0fa60d1dea508
Pablo Cubillos <pcubillos326@gmail.com>
2024-03-14 08:18:59 -0500

    adding form and validations#

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup>
-
+import Form from './components/Form.vue'
 </script>
 
-<template></template>
+<template>
+  <h1 class="title">Weather Finder</h1>
+  <div class="container weather-search">
+
+    <Form />
+  </div>
+</template>
 
 <style scoped></style>
---
+++ src/components/Alert.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="alert">
+    <slot></slot>
+  </div>
+</template>
+
+<style>
+.alert {
+  text-align: center;
+  margin-bottom: 2rem;
+  color: var(--white);
+  text-transform: uppercase;
+  font-weight: 900;
+  font-size: 1.8rem;
+  padding: .5rem
+}
+</style>
